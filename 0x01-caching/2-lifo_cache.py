585ffbe9dc5bdeafcacf904f06c5bd33da5a842a
essaket <hichamessaket17@gmail.com>
2024-08-20 09:22:08 -0700

    Writing task 2

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -8,17 +8,19 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """Constructor"""
         super().__init__()
+        self.last = None
 
     def put(self, key, item):
         """Add an item in the cache"""
         if key is None or item is None:
             return
         if key is not None and item is not None:
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                last_key = next(reversed(self.cache_data))
-                del self.cache_data[last_key]
-                print("DISCARD: {}".format(last_key))
             self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                last = self.last
+                del self.cache_data[last]
+                print("DISCARD: {}".format(last))
+            self.last = key
 
     def get(self, key):
         """Return the value in self.cache_data linked to key"""
