f15a4c9013db99769655bfb409241f11b580bc7f
essaket <hichamessaket17@gmail.com>
2024-08-27 12:50:05 -0700

    Writing task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""3. Parametrize templates"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Configuration settings for Babel and supported languages"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Select the best language match based on
+       the request's Accept-Language header"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def index():
+    """Render the '3-index.html' template for the root URL ("/")"""
+    return render_template("3-index.html")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
