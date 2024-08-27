136fa5277898accb19a1504bdb125994338ef7ae
Hicham Essaket <132906055+essaket@users.noreply.github.com>
2024-08-27 21:02:45 +0100

    Update 3-app.py

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-"""3. Parametrize templates"""
+""" doc doc doc """
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
-class Config:
-    """Configuration settings for Babel and supported languages"""
+class Config(object):
+    """doc doc doc"""
+
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -13,22 +14,20 @@ class Config:
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale() -> str:
-    """Select the best language match based on
-       the request's Accept-Language header"""
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    """doc doc doc"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route("/")
-def index():
-    """Render the '3-index.html' template for the root URL ("/")"""
+def index() -> str:
+    """doc doc doc"""
     return render_template("3-index.html")
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
